2dd88653cadd56cef1af7d2f4a3582d788d1c695
“Gimena” <gimenapascuale@gmail.com>
2024-11-19 00:44:00 -0300

    Deploy

--- back/config/middlewares.ts
+++ back/config/middlewares.ts
@@ -2,7 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://mi-app.vercel.app'],  // Dominio de tu frontend en Vercel
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
